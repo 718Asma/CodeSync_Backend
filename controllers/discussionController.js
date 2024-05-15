94e3ed1d72b3bb340f31feebc3c7391358bc2773
Talel Turki <talelturki75@gmail.com>
2024-05-15 22:52:55 +0100

    added a check for discussion banner and successfully tested the controllers

--- controllers/discussionController.js
+++ controllers/discussionController.js
@@ -42,8 +42,12 @@ exports.discussion_post = [
         creator,
         title,
         description,
-        banner, // Assign the banner path to the discussion
+        // banner,
       });
+
+      if (banner != null || banner != undefined) {
+        discussion.banner = banner; // Assign the banner path to the discussion
+      }
 
       // Save discussion to database
       await discussion.save();
@@ -80,7 +84,7 @@ exports.get_discussions_by_name = [
   passport.authenticate("jwt", { session: false }),
   query("name")
     .trim()
-    .isLength({ min: 5, max: 50 })
+    .isLength({ min: 3, max: 50 })
     .withMessage("Name must be between 5 and 50 characters long"),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
